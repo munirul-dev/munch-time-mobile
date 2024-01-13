5e346cf5f15bc0bd560ace2d97c3958bfe58c35e
Munirul Haikal Bin Hasbulmunir <munirulhaikal@gmail.com>
2024-01-13 18:19:08 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'munch-time-mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
