9196b1ada198a6112c2e83d653d139bb8b7cb927
Munirul Haikal Bin Hasbulmunir <munirulhaikal@gmail.com>
2024-02-05 14:15:19 +0800

    Update environment and assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  appName: 'MunchTime',
+  yearReleased: 2024,
+  server: 'https://munch-time-web.test',
+  api: '/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appName: 'MunchTime',
+  yearReleased: 2024,
+  server: 'https://munch-time-web.test',
+  api: '/api/',
 };
 
 /*
