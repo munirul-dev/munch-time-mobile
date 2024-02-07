0f4eadabdeed4b48b1d1acb5348f170859c787a4
Munirul Haikal Bin Hasbulmunir <munirulhaikal@gmail.com>
2024-02-07 16:23:37 +0800

    Update reservation process

--- src/app/pages/reservation/reservation-confirmation/reservation-confirmation.page.ts
+++ src/app/pages/reservation/reservation-confirmation/reservation-confirmation.page.ts
@@ -1,15 +1,117 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { concatMap } from 'rxjs';
+import { Menu } from 'src/app/model/menu.model';
+import { Student } from 'src/app/model/student.model';
+import { MenuService } from 'src/app/services/menu.service';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-reservation-confirmation',
   templateUrl: './reservation-confirmation.page.html',
   styleUrls: ['./reservation-confirmation.page.scss'],
 })
-export class ReservationConfirmationPage implements OnInit {
+export class ReservationConfirmationPage {
+  isLoading: boolean = true;
+  selectedStudent!: Student;
+  selectedStudentId: number = 0;
+  selectedMenu!: Menu;
+  selectedMenuId: number = 0;
+  totalQuantity: number = 1;
+  totalAmount: number = 0;
+  todayDate: Date = new Date();
 
-  constructor() { }
+  constructor(
+    private menuService: MenuService,
+    private studentService: StudentService,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private activatedRoute: ActivatedRoute,
+  ) {
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.selectedStudentId = Number(params.get('childId'));
+      this.selectedMenuId = Number(params.get('menuId'));
+      this.studentService.edit({
+        id: params.get('childId'),
+      }).pipe(
+        concatMap((student: any) => {
+          this.selectedStudent = student.data;
+          return this.menuService.edit({ id: params.get('menuId') });
+        }),
+      ).subscribe({
+        next: (menu: any) => {
+          this.selectedMenu = menu.data;
+        }, error: (error: any) => {
+          this.showToast('An error occurred, please try again later!', 2000, 'warning');
+        }
+      });
+    });
+  }
 
-  ngOnInit() {
+  increaseCount() {
+    this.totalQuantity++;
+    this.totalAmount = this.selectedMenu.price * this.totalQuantity;
+  }
+
+  decreaseCount() {
+    if (this.totalQuantity > 1) {
+      this.totalQuantity--;
+      this.totalAmount = this.selectedMenu.price * this.totalQuantity;
+    }
+  }
+
+  confirmReservation() {
+    this.alertController.create({
+      header: 'Confirm Reservation',
+      message: 'Are you sure you want to confirm this reservation?',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            this.isLoading = true;
+          }
+        },
+      ]
+    }).then(alertElement => {
+      alertElement.present();
+    });
+  }
+
+  private showAlert(title: string, text: string) {
+    this.alertController.create({
+      header: title,
+      message: text,
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'cancel',
+        }
+      ]
+    }).then(alertElement => {
+      alertElement.present();
+    });
+  }
+
+  private showToast(text: string, time: number, buttonIcon: string) {
+    this.toastController.create({
+      message: text,
+      duration: time,
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: buttonIcon,
+          role: 'cancel',
+        }
+      ]
+    }).then(toastElement => {
+      toastElement.present();
+    });
   }
 
 }
--- src/app/pages/reservation/reservation-select/reservation-select.page.ts
+++ src/app/pages/reservation/reservation-select/reservation-select.page.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Student } from 'src/app/model/student.model';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-reservation-select',
   templateUrl: './reservation-select.page.html',
   styleUrls: ['./reservation-select.page.scss'],
 })
-export class ReservationSelectPage implements OnInit {
+export class ReservationSelectPage implements OnInit, OnDestroy {
+  isLoading: boolean = true;
+  loadedStudent: Student[] = [];
+  private studentSub!: Subscription;
 
-  constructor() { }
+  constructor(
+    private studentService: StudentService,
+    private toastController: ToastController,
+    private alertController: AlertController,
+  ) { }
 
   ngOnInit() {
+    this.studentSub = this.studentService.studentList.subscribe(data => {
+      this.loadedStudent = data;
+    });
+  }
+
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.studentService.index().subscribe({
+      next: (response: any) => {
+        this.isLoading = false;
+      }, error: (error: any) => {
+        this.isLoading = false;
+        this.showToast('An error occurred, please try again later!', 2000, 'warning');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.studentSub) {
+      this.studentSub.unsubscribe();
+    }
+  }
+
+  private showToast(text: string, time: number, buttonIcon: string) {
+    this.toastController.create({
+      message: text,
+      duration: time,
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: buttonIcon,
+          role: 'cancel',
+        }
+      ]
+    }).then(toastElement => {
+      toastElement.present();
+    });
+  }
+
+  private showAlert(title: string, text: string) {
+    this.alertController.create({
+      header: title,
+      message: text,
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'cancel'
+        }
+      ]
+    }).then(toastElement => {
+      toastElement.present();
+    });
   }
 
 }
